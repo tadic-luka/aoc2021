79eedd552005e3381cc55d3c7e1db3e651cec336
Luka Tadic <luka.tadic365@gmail.com>
2021-12-11 23:22:19 +0100

    extract same operations to a method in day07 solutions

--- src/solution/day07.rs
+++ src/solution/day07.rs
@@ -1,43 +1,37 @@
+fn get_distances<'input>(
+    positions: &'input [usize],
+    target: usize,
+) -> impl Iterator<Item = usize> + 'input {
+    positions.iter().copied().map(move |pos| {
+        if pos < target {
+            target - pos
+        } else {
+            pos - target
+        }
+    })
+}
 fn move_to_pos_cost_linear(positions: &[usize], target: usize) -> usize {
-    positions
-        .iter()
-        .copied()
-        .map(|pos| {
-            if pos < target {
-                target - pos
-            } else {
-                pos - target
-            }
-        })
-        .sum()
+    get_distances(positions, target).sum()
 }
 
 fn move_to_pos_cost_non_linear(positions: &[usize], target: usize) -> usize {
-    positions
-        .iter()
-        .copied()
-        .map(|pos| {
-            if pos < target {
-                target - pos
-            } else {
-                pos - target
-            }
-        }).map(|dist| dist * (dist + 1) / 2)
+    get_distances(positions, target)
+        .map(|dist| dist * (dist + 1) / 2)
         .sum()
 }
 
 pub fn solve_puzzle_one(positions: &[usize]) -> usize {
     let max = positions.iter().copied().max().unwrap_or_default();
-    (0..max).fold(usize::MAX, |min, pos| {
-        std::cmp::min(min, move_to_pos_cost_linear(positions, pos))
-    })
+    (0..max)
+        .map(|pos| move_to_pos_cost_linear(positions, pos))
+        .fold(usize::MAX, std::cmp::min)
 }
 
 pub fn solve_puzzle_two(positions: &[usize]) -> usize {
     let max = positions.iter().copied().max().unwrap_or_default();
-    (0..max).fold(usize::MAX, |min, pos| {
-        std::cmp::min(min, move_to_pos_cost_non_linear(positions, pos))
-    })
+    (0..max)
+        .map(|pos| move_to_pos_cost_non_linear(positions, pos))
+        .fold(usize::MAX, std::cmp::min)
 }
 
 #[cfg(test)]
