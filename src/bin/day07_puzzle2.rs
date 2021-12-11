0f5d78190848629ba6dff8a08c4a94880ec1c83c
Luka Tadic <luka.tadic365@gmail.com>
2021-12-11 23:20:06 +0100

    solution day07 puzzle 2

---
+++ src/bin/day07_puzzle2.rs
@@ -0,0 +1,26 @@
+use std::io::{Error, ErrorKind, Result};
+
+use aoc_2021::{get_file, get_input_contents, solution::day07::solve_puzzle_two};
+
+fn main() -> Result<()> {
+    let file = get_file()?;
+    let input = get_input_contents(file)?;
+
+    let input: Vec<usize> = input
+        .trim()
+        .split(',')
+        .enumerate()
+        .map(|(i, val)| {
+            val.parse::<usize>().map_err(|_| {
+                Error::new(
+                    ErrorKind::InvalidData,
+                    format!("Invalid integer provided at index  {}, {}", i, val),
+                )
+            })
+        })
+        .collect::<Result<_>>()?;
+
+    let result = solve_puzzle_two(&input);
+    println!("{}", result);
+    Ok(())
+}
--- src/solution/day07.rs
+++ src/solution/day07.rs
@@ -1,19 +1,43 @@
-fn move_to_pos_cost(positions: &[usize], target: usize) -> usize {
-    positions.iter().copied()
-        .map(|pos| if pos < target {
-            target - pos
-        } else {
-            pos - target
+fn move_to_pos_cost_linear(positions: &[usize], target: usize) -> usize {
+    positions
+        .iter()
+        .copied()
+        .map(|pos| {
+            if pos < target {
+                target - pos
+            } else {
+                pos - target
+            }
         })
         .sum()
 }
 
+fn move_to_pos_cost_non_linear(positions: &[usize], target: usize) -> usize {
+    positions
+        .iter()
+        .copied()
+        .map(|pos| {
+            if pos < target {
+                target - pos
+            } else {
+                pos - target
+            }
+        }).map(|dist| dist * (dist + 1) / 2)
+        .sum()
+}
 
 pub fn solve_puzzle_one(positions: &[usize]) -> usize {
     let max = positions.iter().copied().max().unwrap_or_default();
     (0..max).fold(usize::MAX, |min, pos| {
-            std::cmp::min(min, move_to_pos_cost(positions, pos))
-        })
+        std::cmp::min(min, move_to_pos_cost_linear(positions, pos))
+    })
+}
+
+pub fn solve_puzzle_two(positions: &[usize]) -> usize {
+    let max = positions.iter().copied().max().unwrap_or_default();
+    (0..max).fold(usize::MAX, |min, pos| {
+        std::cmp::min(min, move_to_pos_cost_non_linear(positions, pos))
+    })
 }
 
 #[cfg(test)]
@@ -22,7 +46,13 @@ mod tests {
 
     #[test]
     fn part_1_test() {
-        let input = [16,1,2,0,4,2,7,1,2,14];
+        let input = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
         assert_eq!(solve_puzzle_one(&input), 37);
+    }
+
+    #[test]
+    fn part_2_test() {
+        let input = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
+        assert_eq!(solve_puzzle_two(&input), 168);
     }
 }
