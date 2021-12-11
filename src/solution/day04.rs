28f79578db909d42fd3119180aa98a9e3128a80d
Luka Tadic <luka.tadic365@gmail.com>
2021-12-11 14:33:28 +0100

    solution day04 puzzle 2

--- src/solution/day04.rs
+++ src/solution/day04.rs
@@ -64,6 +64,28 @@ pub fn solve_puzzle_one(numbers: &[usize], mut boards: Vec<Board>) -> usize {
     0
 }
 
+pub fn solve_puzzle_two(numbers: &[usize], mut boards: Vec<Board>) -> usize {
+    let mut has_won = vec![false; boards.len()];
+    let mut solved = 0;
+    let boards_len = boards.len();
+    for &number in numbers {
+        for (i, board) in boards.iter_mut().enumerate() {
+            if has_won[i] {
+                continue;
+            }
+            board.mark_num(number);
+            if board.is_win() {
+                solved += 1;
+                has_won[i] = true;
+            }
+            if solved == boards_len {
+                return board.sum_unmarked() * number;
+            }
+        }
+    }
+    0
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -120,5 +142,29 @@ mod tests {
         board.mark_num(0);
         board.mark_num(8);
         assert!(!board.is_win());
+    }
+
+    #[test]
+    fn part_2_test() {
+        let numbers = [
+            7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19,
+            3, 26, 1,
+        ];
+        let boards = vec![
+            Board::new([
+                22, 13, 17, 11, 0, 8, 2, 23, 4, 24, 21, 9, 14, 16, 7, 6, 10, 3, 18, 5, 1, 12, 20,
+                15, 19,
+            ]),
+            Board::new([
+                3, 15, 0, 2, 22, 9, 18, 13, 17, 5, 19, 8, 7, 25, 23, 20, 11, 10, 24, 4, 14, 21, 16,
+                12, 6,
+            ]),
+            Board::new([
+                14, 21, 17, 24, 4, 10, 16, 15, 9, 19, 18, 8, 23, 26, 20, 22, 11, 13, 6, 5, 2, 0,
+                12, 3, 7,
+            ]),
+        ];
+
+        assert_eq!(solve_puzzle_two(&numbers, boards), 1924);
     }
 }
